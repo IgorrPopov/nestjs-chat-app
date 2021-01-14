b6f03f794a3fca70c698b31ac31b9822155f4bc2
IgorrPopov <ginger_tulips@protonmail.com>
2021-01-14 10:21:03 +0200

    first commit of nestjs-chat-app with boilerplate from nestjs-book-api

---
+++ server/src/app.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+  // imports: [TypeOrmModule.forRoot(typeOrmConfig), BooksModule],
+  // imports: [BooksModule],
+})
+export class AppModule {}
